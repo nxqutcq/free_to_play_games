afef6a187cc7100f7128a642b0b1a68597a435ed
vlad <vnelipovich63@gmail.com>
2024-03-24 12:03:27 +0300

    feat: add minimal structure

---
+++ src/pages/GamePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GamePage: React.FC = () => {
+  return (
+    <div>GamePage</div>
+  )
+}
+
+export default GamePage
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainPaige: React.FC = () => {
+  return (
+    <div>MainPaige</div>
+  )
+}
+
+export default MainPaige
