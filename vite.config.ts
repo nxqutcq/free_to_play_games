a39e8f9c109d27e2bddd19b72ac601e54dc35b31
vlad <vnelipovich63@gmail.com>
2024-03-26 18:12:29 +0300

    fix: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/free_to_play_games',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
