2c3ec7c1aa1b96dd47f745cde31e492861f0e998
vlad <vnelipovich63@gmail.com>
2024-03-27 19:22:33 +0300

    feat: add nav links

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,12 +7,24 @@ import { Separator } from '@/components/ui/separator'
 const Header: React.FC = () => (
   <header className="flex h-20 w-full mx-auto items-center flex-col mb-5">
     <div className="flex items-center w-[1200px] justify-between h-full flex-row">
-      <nav>
-        <NavLink draggable={false} to="/" role="link">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <nav className="flex flex-row items-center justify-between w-full">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          <NavLink draggable={false} to="/" role="link">
             FreeToGame
-          </h1>
-        </NavLink>
+          </NavLink>
+        </h1>
+        <ul className="flex items-center mr-10 w-[200px] justify-between">
+          <li>
+            <NavLink draggable={false} to="/" role="link">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink draggable={false} to="/games" role="link">
+              Games
+            </NavLink>
+          </li>
+        </ul>
       </nav>
       <ModeToggle />
     </div>
