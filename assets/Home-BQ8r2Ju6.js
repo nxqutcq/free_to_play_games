0dfa29a8d342de481a63e95c6a4f75c95fa382bf
vlad <vnelipovich63@gmail.com>
2024-03-29 14:25:09 +0300

    Updates

---
+++ assets/Home-BQ8r2Ju6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CWzh8P8s.js";const m=()=>e.jsx("main",{className:"mx-auto flex min-h-screen overflow-x-hidden items-center w-[1200px] mt-2 p-5;"});export{m as default};
--- assets/NotFound-DV0I39UV.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DKYNEqbb.js";const s=()=>e.jsx("div",{className:"flex min-h-screen w-[1200px] justify-center items-center",children:"NotFound"});export{s as default};
