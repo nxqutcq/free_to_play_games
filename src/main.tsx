05be3812aaea7c16bd0cd3fd0d6225eaca127fe8
vlad <vnelipovich63@gmail.com>
2024-03-29 18:59:54 +0300

    fix: routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/free2play-games.netlify.app">
+    <BrowserRouter basename="/">
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/free2play-games',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
