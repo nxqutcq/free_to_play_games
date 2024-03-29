d4ffaa541f79d07c822ac8fe680a8ec1b7524c66
vlad <vnelipovich63@gmail.com>
2024-03-29 16:36:01 +0300

    refactor: delete gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/free_to_play_games',
+  base: '/https://free2playgame.netlify.app',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
