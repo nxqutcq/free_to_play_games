76fd43746c5b9e33ce381bc0c8cfb9f205b2952d
vlad <vnelipovich63@gmail.com>
2024-03-30 12:51:32 +0300

    feat: add frequently asked questions to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import FrequentlyAskedQuestions from '@/components/FrequentlyAskedQuestions'
+
 const Home: React.FC = () => {
   return (
-    <main className="mx-auto flex min-h-screen overflow-x-hidden items-center w-[1200px] mt-2 p-5;"></main>
+    <main className="mx-auto flex min-h-screen overflow-x-hidden items-center mt-2 p-5;">
+      <div>
+        <div>main</div>
+        <div>
+          <FrequentlyAskedQuestions />
+        </div>
+      </div>
+    </main>
   )
 }
 
