36ccf142b96cc0042b19a2547c27d4af1f74ccd0
vlad <vnelipovich63@gmail.com>
2024-04-01 19:37:18 +0300

    refactor: extract lazy components

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,10 @@
-import { lazy, Suspense } from 'react'
+import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
+
+import { Home, GamePage, GamesList, NotFound } from './LazyComponents'
 
 import ScrollToTopOnPageChange from '@/components/ScrollToTopOnPageChange.tsx'
 import Loader from '@/components/shared/Loader.tsx'
-
-const Home = lazy(() => import('../pages/Home.tsx'))
-const GamePage = lazy(() => import('../pages/GamePage.tsx'))
-const GamesList = lazy(() => import('../pages/GamesList.tsx'))
-const NotFound = lazy(() => import('../pages/NotFound.tsx'))
 
 export const AppRoutes: React.FC = () => (
   <div className="flex min-h-screen xs2:w-[400px] xs:w-[320px] justify-center items-center xl:w-[1280px] sm:w-[640px] lg:w-[1024px] md:w-[768px]">
---
+++ src/routes/LazyComponents.tsx
@@ -0,0 +1,6 @@
+import { lazy } from 'react'
+
+export const Home = lazy(() => import('../pages/Home.tsx'))
+export const GamePage = lazy(() => import('../pages/GamePage.tsx'))
+export const GamesList = lazy(() => import('../pages/GamesList.tsx'))
+export const NotFound = lazy(() => import('../pages/NotFound.tsx'))
