d62c4395e9fc78fb2bc9d5621ec7c29f26acea67
vlad <vnelipovich63@gmail.com>
2024-04-09 14:57:58 +0300

    feat: add styles for header dropdown menu

--- src/components/FreeGamesMenu.tsx
+++ src/components/FreeGamesMenu.tsx
@@ -20,9 +20,9 @@ const HeaderFreeGamesMenu: React.FC = () => (
         <NavigationMenuContent>
           {GAME_TYPES.map((gameType, index) => (
             <React.Fragment key={index}>
-              <NavigationMenuLink className="flex w-[250px]">
+              <NavigationMenuLink className="flex w-[220px]">
                 <NavLink
-                  className="p-5 flex w-[full] justify-center h-[38px] items-center flex-row "
+                  className="min-h-[35px] w-[220px] block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                   draggable={false}
                   to={gameType.url}
                   role="link"
