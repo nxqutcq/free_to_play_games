164f6d4212f39c40e2668df8ff6c78a6a879dc41
vlad <vnelipovich63@gmail.com>
2024-04-09 14:08:15 +0300

    fix: add check for existence property

--- src/components/SystemRequirements.tsx
+++ src/components/SystemRequirements.tsx
@@ -4,18 +4,22 @@ import { requirements } from '@/constants'
 import { SystemRequirementsProps } from '@/types/games'
 
 const SystemRequirements: React.FC<SystemRequirementsProps> = ({ data }) => {
+  if (!data.minimum_system_requirements) {
+    return null
+  }
   return (
     <div>
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight sm:flex">
         Minimum System Requirements:
       </h4>
       <ul className="mb-5 my-6 ml-6 list-disc [&>li]:mt-2  sm:flex sm:flex-col">
-        {requirements.map((req, index) => (
-          <li key={index}>
-            {req.name}:{' '}
-            {data.minimum_system_requirements[req.key] || 'Not specified'}
-          </li>
-        ))}
+        {requirements &&
+          requirements.map((req) => (
+            <li key={req.key}>
+              {req.name}:{' '}
+              {data.minimum_system_requirements[req.key] || 'Not specified'}
+            </li>
+          ))}
       </ul>
     </div>
   )
