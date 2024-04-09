f36429ee86d4f342828941f0a0ad84281c48ad57
vlad <vnelipovich63@gmail.com>
2024-04-09 15:06:25 +0300

    refactor: delete logo padding

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -9,8 +9,8 @@ const Header: React.FC = () => {
   return (
     <header className="flex h-20 w-full mx-auto items-center flex-col mb-5 xs:mb-2">
       <div className="flex xs2:w-[400px] items-center xs:w-[320px] xl:w-[1200px] lg:w-[1024px] justify-between h-full flex-row sm:w-[640px] md:w-[768px] sm:pr-8 xs:pr-5 xs3:w-[480px]">
-        <nav className="flex flex-row items-center justify-between w-full sm:pl-5 sm:pr-5 xs:pl-5">
-          <h1 className="xs:hidden scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl sm:flex sm:pl-3">
+        <nav className="flex flex-row items-center justify-between w-full sm:pr-5">
+          <h1 className="xs:hidden scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl sm:flex">
             <NavLink draggable={false} to="/" role="link">
               FreeToGame
             </NavLink>
