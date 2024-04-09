7d3d6e868f0cac7bcff32c63fefd56a6da76888f
vlad <vnelipovich63@gmail.com>
2024-04-09 14:36:34 +0300

    refactor: change max width

--- src/components/GameInfo.tsx
+++ src/components/GameInfo.tsx
@@ -10,7 +10,7 @@ import { GameInfoProps } from '@/types/games'
 const GameInfo: React.FC<GameInfoProps> = ({ data, goBack }) => {
   usePageTitle(`${data?.title} Download and Play`)
   return (
-    <article className="xs3:w-[480px] xs4:w-[425px] xs1:w-[375px] xs2:w-[400px] min-h-screen mb-10 xl:w-[1280px] lg:w-[1024px] overflow-hidden shadow-lg sm:p-6 sm:flex sm:flex-col sm:items-start sm:w-[640px] md:w-[768px] xs:w-[320px] xs:p-2">
+    <article className="xs3:w-[480px] xs4:w-[425px] xs1:w-[375px] xs2:w-[400px] min-h-screen mb-10 xl:w-[1200px] lg:w-[1024px] overflow-hidden shadow-lg sm:p-6 sm:flex sm:flex-col sm:items-start sm:w-[640px] md:w-[768px] xs:w-[320px] xs:p-2">
       <GameDescription data={data} goBack={goBack} />
       <SystemRequirements data={data} />
       <Screenshots data={data} />
