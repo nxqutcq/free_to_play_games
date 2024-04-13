0b269704b687d49ab9ae50d47ff249ce1e081954
vlad <vnelipovich63@gmail.com>
2024-04-13 14:44:18 +0300

    refactor: сhange the background color and text color of the links

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,10 +7,10 @@ import { Separator } from '@/components/ui/separator'
 
 const Header: React.FC = () => {
   return (
-    <header className="flex h-20 w-full mx-auto items-center flex-col mb-5 xs:mb-2">
+    <header className="bg-[rgb(23,23,23)] flex h-20 w-full mx-auto items-center flex-col mb-5 xs:mb-2">
       <div className="flex xs2:w-[400px] items-center xs:w-[320px] xl:w-[1200px] lg:w-[1024px] justify-between h-full flex-row sm:w-[640px] md:w-[768px] xs3:w-[480px]">
         <nav className="flex flex-row items-center justify-between w-full sm:pr-5">
-          <h1 className="xs:hidden scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl sm:flex">
+          <h1 className="xs:hidden text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl sm:flex">
             <NavLink draggable={false} to="/" role="link">
               FreeToGame
             </NavLink>
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
           <ul className="flex items-center mr-10 w-[200px] justify-between">
             <li>
               <NavLink
-                className="flex rounded-md p-3 leading-none transition-colors hover:bg-accent"
+                className=" bg-background flex rounded-md p-3 leading-none transition-colors hover:bg-accent"
                 draggable={false}
                 to="/"
                 role="link"
