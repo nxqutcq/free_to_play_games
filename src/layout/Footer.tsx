47d8464a39209c859c06e101de6f7c79d910c5af
vlad <vnelipovich63@gmail.com>
2024-04-15 17:33:05 +0300

    refactor: improve the semantic structure of the footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -11,42 +11,66 @@ import { Separator } from '@/components/ui/separator'
 const Footer: React.FC = () => (
   <footer className="flex w-full mx-auto items-center flex-col">
     <Separator />
-    <address className="h-[280px] flex flex-col w-full xl:w-[1200px] not-italic">
+    <div className="h-[280px] flex flex-col w-full xl:w-[1200px] not-italic">
       <div className="flex h-[200px] items-center gap-[140px]">
         <div className="flex flex-row h-full items-center">
-          <div className="w-[200px] items-start flex flex-col gap-3">
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              About Us
-            </p>
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              API
-            </p>
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Contact Us
-            </p>
-          </div>
-          <div className="w-[200px] items-start flex flex-col gap-3">
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Help/FAQ
-            </p>
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Support & Bugs
-            </p>
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Sitemap
-            </p>
-          </div>
-          <div className="w-[200px] items-start flex flex-col gap-3">
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Privacy Policy
-            </p>
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Cookies Policy
-            </p>
-            <p className="hover:text-gray-500 transition-colors cursor-pointer">
-              Terms of Use
-            </p>
-          </div>
+          <nav>
+            <ul className="w-[200px] items-start flex flex-col gap-3">
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  About Us
+                </p>
+              </li>
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  API
+                </p>
+              </li>
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Contact Us
+                </p>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul className="w-[200px] items-start flex flex-col gap-3">
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Help/FAQ
+                </p>
+              </li>
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Support & Bugs
+                </p>
+              </li>
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Sitemap
+                </p>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul className="w-[200px] items-start flex flex-col gap-3">
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Privacy Policy
+                </p>
+              </li>
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Cookies Policy
+                </p>
+              </li>
+              <li>
+                <p className="hover:text-gray-500 transition-colors cursor-pointer">
+                  Terms of Use
+                </p>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
       <div className="flex w-full mb-5">
@@ -57,28 +81,31 @@ const Footer: React.FC = () => (
           © 2024 All rights reserved. All trademarks are property of their
           respective owners.
         </p>
-        <div className="flex flex-row gap-10 items-center">
-          <a
-            draggable={false}
-            target="_blank"
-            rel="noopener noreferrer"
-            href={'https://github.com/Vladnel671/free_to_play_games'}
-            className="w-[25px] cursor-pointer"
-          >
-            <GithubIcon className={'github'} />
-          </a>
-          <div className="w-[25px] cursor-pointer">
-            <YouTubeIcon className={'youtube'} />
-          </div>
-          <div className="w-[25px] cursor-pointer">
-            <TwitterIcon className={'twitter'} />
-          </div>
-          <div className="w-[25px] cursor-pointer">
-            <FacebookIcon className={'facebook'} />
-          </div>
-        </div>
+        <address>
+          <ul className="flex flex-row gap-10 items-center">
+            <li className="w-[25px] cursor-pointer">
+              <a
+                draggable={false}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={'https://github.com/Vladnel671/free_to_play_games'}
+              >
+                <GithubIcon className={'github'} />
+              </a>
+            </li>
+            <li className="w-[25px] cursor-pointer">
+              <YouTubeIcon className={'youtube'} />
+            </li>
+            <li className="w-[25px] cursor-pointer">
+              <TwitterIcon className={'twitter'} />
+            </li>
+            <li className="w-[25px] cursor-pointer">
+              <FacebookIcon className={'facebook'} />
+            </li>
+          </ul>
+        </address>
       </div>
-    </address>
+    </div>
   </footer>
 )
 
