a1af137bf1fbfc3000a96baf64d2f0259614b047
vlad <vnelipovich63@gmail.com>
2024-04-16 18:17:29 +0300

    refactor: change to 4 columns

--- src/components/GamesBunch.tsx
+++ src/components/GamesBunch.tsx
@@ -5,7 +5,7 @@ import GamesBunchItem from './GamesBunchItem'
 import { GamesBunchProps } from '@/types/games'
 
 const GamesBunch: React.FC<GamesBunchProps> = ({ data }) => (
-  <div className="xl:gap-0 justify-between overflow-x-hidden flex flex-wrap items-start gap-2 md:gap-5 lg:gap-2 xl:w-[1200px] flex-row">
+  <div className="grid grid-cols-4 gap-3 xl:w-[1200px] flex-row">
     {data?.map((game) => <GamesBunchItem key={game.id} game={game} />)}
   </div>
 )
