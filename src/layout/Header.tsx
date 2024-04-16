b4c02148a9b2833aa1efbc5f33510dc7597c5206
vlad <vnelipovich63@gmail.com>
2024-04-16 18:15:40 +0300

    refactor: change logo size

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
     <header className="bg-gray-950 flex h-[4rem] w-full mx-auto items-center flex-col mb-5">
       <div className="flex xs2:w-[400px] items-center xs:w-[300px] xl:w-[1200px] lg:w-[1024px] justify-between h-full flex-row sm:w-[640px] md:w-[768px] xs3:w-[480px]">
         <nav className="flex flex-row items-center justify-between w-full sm:pr-5">
-          <h1 className="xs:hidden text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl sm:flex">
+          <h1 className="xs:hidden text-white scroll-m-20 font-extrabold tracking-tight text-4xl sm:flex">
             <NavLink draggable={false} to="/" role="link">
               FreeToGame
             </NavLink>
