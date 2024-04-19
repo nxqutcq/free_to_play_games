c04809ad3ab7a570236e5292e4008261d13d6305
vlad <vnelipovich63@gmail.com>
2024-04-19 18:39:03 +0300

    fix: react helmet

--- src/components/GamesCount.tsx
+++ src/components/GamesCount.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import { GamesCountProps } from '@/types/games'
 
 const GamesCount: React.FC<GamesCountProps> = ({ gamesCount, category }) => (
-  <div className="mt-10 sm:px-5 xl:px-0 xl:w-[1200px]">
-    <Helmet>
-      <title>{`${gamesCount} Free-to-play ${category} games found in our games list!`}</title>
-    </Helmet>
-    <div>
-      <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        Top Free Games for PC and Browser In {new Date().getFullYear()}!
-      </h2>
+  <HelmetProvider>
+    <div className="mt-10 sm:px-5 xl:px-0 xl:w-[1200px]">
+      <Helmet>
+        <title>{`${gamesCount} Free-to-play ${category} games found in our games list!`}</title>
+      </Helmet>
+      <div>
+        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Top Free Games for PC and Browser In {new Date().getFullYear()}!
+        </h2>
+      </div>
+      <div>
+        <p className="mb-5 flex gap-2">
+          {gamesCount} {category?.toLocaleUpperCase()} games found in our games
+          list!
+        </p>
+      </div>
     </div>
-    <div>
-      <p className="mb-5 flex gap-2">
-        {gamesCount} {category?.toLocaleUpperCase()} games found in our games
-        list!
-      </p>
-    </div>
-  </div>
+  </HelmetProvider>
 )
 export default GamesCount
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { ChevronRight } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
-import { Helmet } from 'react-helmet'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 import FrequentlyAskedQuestions from '@/components/FAQ'
@@ -39,56 +39,58 @@ const Home: React.FC = () => {
   if (!data) return <NoDataComponent goBack={goBack} />
 
   return (
-    <main className="justify-center flex min-h-screen items-start">
-      <Helmet>
-        <title>Discover the best free-to-play games!</title>
-        <meta
-          name="description"
-          content="Track what you've played and search for what to play next!"
-        />
-        <meta
-          property="og:title"
-          content="Discover the best free-to-play games!"
-        />
-        <meta
-          property="og:description"
-          content="Track what you've played and search for what to play next!"
-        />
-        <meta
-          property="og:image"
-          content="https://psv4.userapi.com/c909618/u250373309/docs/d49/951cd3d248ca/games.png?extra=1hDPGpaBgpde0QRpFSGjRgrCvFiKOgtkwFA1Qmq37c2HlvpvO6RxcyhbMNPLlySSqC9zrFmEocAoQbfRzPf2iUP__cLAy5MisGXxb_pO_NyHzPsQs2yjqHf5jlV-qrSWP8AbePpJWiCJWJMT76ws-zk"
-        />
-      </Helmet>
+    <HelmetProvider>
+      <main className="justify-center flex min-h-screen items-start">
+        <Helmet>
+          <title>Discover the best free-to-play games!</title>
+          <meta
+            name="description"
+            content="Track what you've played and search for what to play next!"
+          />
+          <meta
+            property="og:title"
+            content="Discover the best free-to-play games!"
+          />
+          <meta
+            property="og:description"
+            content="Track what you've played and search for what to play next!"
+          />
+          <meta
+            property="og:image"
+            content="https://psv4.userapi.com/c909618/u250373309/docs/d49/951cd3d248ca/games.png?extra=1hDPGpaBgpde0QRpFSGjRgrCvFiKOgtkwFA1Qmq37c2HlvpvO6RxcyhbMNPLlySSqC9zrFmEocAoQbfRzPf2iUP__cLAy5MisGXxb_pO_NyHzPsQs2yjqHf5jlV-qrSWP8AbePpJWiCJWJMT76ws-zk"
+          />
+        </Helmet>
 
-      <div className="flex flex-col w-full items-center">
-        <Jumbotron />
-        <div className="w-[1200px] mt-5 flex flex-col mb-2">
-          <div className="min-h-[250px] mb-10">
-            <Recommendations data={randomGames} />
-          </div>
-          <div className="flex min-h-[500px] justify-between mb-[80px]">
-            <div className="w-[800px]">
-              <NewReleases data={firstSevenGames || []} />
-              <div className="flex justify-end mt-5">
-                <NavLink
-                  to={'/games'}
-                  className="border w-[max-content] rounded-md p-3 leading-none transition-colors hover:bg-accent flex items-center justify-center gap-2"
-                >
-                  More Games
-                  <ChevronRight />
-                </NavLink>
+        <div className="flex flex-col w-full items-center">
+          <Jumbotron />
+          <div className="w-[1200px] mt-5 flex flex-col mb-2">
+            <div className="min-h-[250px] mb-10">
+              <Recommendations data={randomGames} />
+            </div>
+            <div className="flex min-h-[500px] justify-between mb-[80px]">
+              <div className="w-[800px]">
+                <NewReleases data={firstSevenGames || []} />
+                <div className="flex justify-end mt-5">
+                  <NavLink
+                    to={'/games'}
+                    className="border w-[max-content] rounded-md p-3 leading-none transition-colors hover:bg-accent flex items-center justify-center gap-2"
+                  >
+                    More Games
+                    <ChevronRight />
+                  </NavLink>
+                </div>
+              </div>
+              <div className="w-[370px]">
+                <MostPlayedToday />
               </div>
             </div>
-            <div className="w-[370px]">
-              <MostPlayedToday />
+            <div className="flex w-full">
+              <FrequentlyAskedQuestions />
             </div>
           </div>
-          <div className="flex w-full">
-            <FrequentlyAskedQuestions />
-          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </HelmetProvider>
   )
 }
 
