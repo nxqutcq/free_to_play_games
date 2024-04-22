a6c24cdc279338fe79faab1e58549f576c065d50
vlad <vnelipovich63@gmail.com>
2024-04-22 18:53:28 +0300

    refactor: restore transition

--- src/components/shared/GameImage.tsx
+++ src/components/shared/GameImage.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { GameImageType } from '@/types/games'
 
-const GameImage: React.FC<GameImageType> = ({ src, alt }) => (
-  <img
-    className="w-full h-full rounded-md"
-    src={src}
-    alt={alt}
-    loading="lazy"
-  />
-)
+const GameImage: React.FC<GameImageType> = ({ src, alt }) => {
+  const [loaded, setLoaded] = useState(false)
+
+  const handleImageLoad = () => {
+    setLoaded(true)
+  }
+
+  return (
+    <img
+      className={`w-full h-full rounded-md transition-opacity duration-300 ${
+        loaded ? 'opacity-100' : 'opacity-0'
+      }`}
+      src={src}
+      alt={alt}
+      onLoad={handleImageLoad}
+    />
+  )
+}
 
 export default GameImage
