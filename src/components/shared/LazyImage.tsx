7adac2aec364e6f21db34769f588514935dc4ed1
vlad <vnelipovich63@gmail.com>
2024-04-23 23:05:03 +0300

    feat: add intersection observer for lazy img

--- src/components/shared/LazyImage.tsx
+++ src/components/shared/LazyImage.tsx
@@ -1,15 +1,37 @@
-import React, { lazy } from 'react'
+import React, { useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 import { Skeleton } from '@/components/ui/skeleton'
 import { GameImageType } from '@/types/games'
 
-const LazyGameImage = lazy(() => import('./GameImage'))
-const LazyImage = (props: GameImageType) => (
-  <React.Suspense
-    fallback={<Skeleton className="absolute inset-0 flex w-full" />}
-  >
-    <LazyGameImage {...props} />
-  </React.Suspense>
-)
+const LazyImage: React.FC<GameImageType> = ({ src, alt }) => {
+  const [loaded, setLoaded] = useState(false)
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  })
+
+  const handleImageLoad = () => {
+    setLoaded(true)
+  }
+
+  return (
+    <div ref={ref}>
+      {inView ? (
+        <img
+          className={`rounded-md game-img transition-opacity duration-300 ${
+            loaded ? 'opacity-100' : 'opacity-0'
+          }`}
+          src={src}
+          alt={alt}
+          onLoad={handleImageLoad}
+        />
+      ) : (
+        <Skeleton className="absolute inset-0 flex w-full" />
+      )}
+    </div>
+  )
+}
 
 export default LazyImage
