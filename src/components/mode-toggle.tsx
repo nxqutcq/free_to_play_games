229377ad23c7fd7cb7e7cd130ae5ce825855afa2
vlad <vnelipovich63@gmail.com>
2024-04-28 00:11:40 +0300

    fix: layout

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -15,8 +15,12 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="bg-stone-900 rounded-xl" variant="outline" size="sm">
-          <Sun className="h-[0.8rem] w-[0.8rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Button
+          className="bg-stone-900 rounded-xl hover:bg-stone-800"
+          variant="outline"
+          size="sm"
+        >
+          <Sun className="h-[0.8rem] w-[0.8rem] text-white rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[0.8rem] w-[0.8rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
