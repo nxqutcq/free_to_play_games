77fa794f84d4f2665142596076a498ebb31e6308
vlad <vnelipovich63@gmail.com>
2024-05-21 18:28:53 +0300

    feat: add shadcn toast

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import App from './App.tsx'
 import './styles/global.scss'
 import ErrorFallback from './components/ErrorFallback.tsx'
 
+import { Toaster } from '@/components/ui/toaster'
+
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 5, retryDelay: 30000 } },
 })
@@ -19,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter basename="/">
         <QueryClientProvider client={queryClient}>
           <App />
+          <Toaster />
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </BrowserRouter>
