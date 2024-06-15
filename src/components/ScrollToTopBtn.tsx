50fc85b5f0c13880f3d359044b62e1028aa1d66c
Vladnel671 <vnelipovich63@gmail.com>
2024-06-15 13:25:39 +0300

    feat: add scrolltotop button

---
+++ src/components/ScrollToTopBtn.tsx
@@ -0,0 +1,40 @@
+import { ArrowUp } from 'lucide-react'
+import { useState, useEffect } from 'react'
+
+import { scrollToTop } from '@/utils'
+
+const ScrollToTopBtn = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    }
+
+    window.addEventListener('scroll', toggleVisibility)
+    return () => window.removeEventListener('scroll', toggleVisibility)
+  }, [])
+
+  return (
+    <div
+      className="bg-background p-3.5 border rounded-full hover:bg-accent transition-colors"
+      onClick={scrollToTop}
+      style={{
+        position: 'fixed',
+        bottom: '2rem',
+        right: '2rem',
+        opacity: isVisible ? 1 : 0,
+        visibility: isVisible ? 'visible' : 'hidden',
+        transition: 'opacity 0.5s ease, visibility 0.5s ease',
+      }}
+    >
+      <ArrowUp />
+    </div>
+  )
+}
+
+export default ScrollToTopBtn
--- src/hooks/useScrollToTopOnPageChange.ts
+++ src/hooks/useScrollToTopOnPageChange.ts
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
+import { scrollToTop } from '@/utils'
+
 const useScrollToTopOnPageChange: React.FC = () => {
   const { pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo(0, 0)
+    scrollToTop()
   }, [pathname])
 
   return null
