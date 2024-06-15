3f18153394f7c3824daf4062ab4538fae8b771d4
Vladnel671 <vnelipovich63@gmail.com>
2024-06-15 13:24:34 +0300

    feat: add scrolltotop button

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+import ScrollToTopBtn from '@/components/ScrollToTopBtn'
+
 const Layout: React.FC = () => (
   <div className="flex items-center overflow-hidden flex-col">
     <div className="min-h-screen w-full max-w-[1140px] px-[1rem]">
       <Outlet />
+      <ScrollToTopBtn />
     </div>
   </div>
 )
