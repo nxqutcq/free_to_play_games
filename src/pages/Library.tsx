a22650c21ba8fba6b5f20044f23f552fdef4d8b7
vlad <vnelipovich63@gmail.com>
2024-06-16 17:57:02 +0300

    feat: add react helmet to library component

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { useLocation } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
@@ -23,57 +24,62 @@ const Library: React.FC = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-row  my-10 gap-3 rounded-md">
-      <div className="w-1/3 h-fit rounded-lg gap-y-1 p-3 border flex flex-col">
-        <div
-          className="cursor-pointer rounded-t border-b hover:bg-accent transition-colors p-3"
-          onClick={() => handleSelection('all')}
-        >
-          All My Games
+    <HelmetProvider>
+      <Helmet>
+        <title>Library</title>
+      </Helmet>
+      <div className="flex min-h-screen flex-row  my-10 gap-3 rounded-md">
+        <div className="w-1/3 h-fit rounded-lg gap-y-1 p-3 border flex flex-col">
+          <div
+            className="cursor-pointer rounded-t border-b hover:bg-accent transition-colors p-3"
+            onClick={() => handleSelection('all')}
+          >
+            All My Games
+          </div>
+          <div
+            className="cursor-pointer rounded-t border-b hover:bg-accent transition-colors p-3"
+            onClick={() => handleSelection('playLater')}
+          >
+            Play later
+          </div>
+          <div
+            className="cursor-pointer rounded-t border-b hover:bg-accent transition-colors p-3"
+            onClick={() => handleSelection('currentPlaying')}
+          >
+            Current playing
+          </div>
+          <div
+            className="cursor-pointer rounded-t hover:bg-accent transition-colors border-b p-3"
+            onClick={() => handleSelection('played')}
+          >
+            Played
+          </div>
+          <div
+            className="cursor-pointer rounded-t hover:bg-accent transition-colors border-b p-3"
+            onClick={() => handleSelection('completed')}
+          >
+            Completed
+          </div>
         </div>
-        <div
-          className="cursor-pointer rounded-t border-b hover:bg-accent transition-colors p-3"
-          onClick={() => handleSelection('playLater')}
-        >
-          Play later
-        </div>
-        <div
-          className="cursor-pointer rounded-t border-b hover:bg-accent transition-colors p-3"
-          onClick={() => handleSelection('currentPlaying')}
-        >
-          Current playing
-        </div>
-        <div
-          className="cursor-pointer rounded-t hover:bg-accent transition-colors border-b p-3"
-          onClick={() => handleSelection('played')}
-        >
-          Played
-        </div>
-        <div
-          className="cursor-pointer rounded-t hover:bg-accent transition-colors border-b p-3"
-          onClick={() => handleSelection('completed')}
-        >
-          Completed
+        <div className="w-full border p-3 rounded-lg">
+          {selectedList === 'all' && (
+            <div className="bg-slate-400">All My Games</div>
+          )}
+          {selectedList === 'playLater' && (
+            <div className="bg-red-500">Play Later List</div>
+          )}
+          {selectedList === 'currentPlaying' && (
+            <div className="bg-gray-800">Current Playing List</div>
+          )}
+          {selectedList === 'played' && (
+            <div className="bg-orange-500">Played List</div>
+          )}
+          {selectedList === 'completed' && (
+            <div className="bg-amber-400">Completed List</div>
+          )}
         </div>
       </div>
-      <div className="w-full border p-3 rounded-lg">
-        {selectedList === 'all' && (
-          <div className="bg-slate-400">All My Games</div>
-        )}
-        {selectedList === 'playLater' && (
-          <div className="bg-red-500">Play Later List</div>
-        )}
-        {selectedList === 'currentPlaying' && (
-          <div className="bg-gray-800">Current Playing List</div>
-        )}
-        {selectedList === 'played' && (
-          <div className="bg-orange-500">Played List</div>
-        )}
-        {selectedList === 'completed' && (
-          <div className="bg-amber-400">Completed List</div>
-        )}
-      </div>
-    </div>
+    </HelmetProvider>
   )
 }
 
